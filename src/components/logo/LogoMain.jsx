836876f9a14b8a43f5ee8d89031046ee0c4cbfc1
pqthinh <phamquangquang2008@gmail.com>
2025-02-16 03:37:22 +0700

    change logo

--- src/components/logo/LogoMain.jsx
+++ src/components/logo/LogoMain.jsx
@@ -1,6 +1,6 @@
 // material-ui
-import { useTheme } from '@mui/material/styles';
-
+// import { useTheme } from '@mui/material/styles';
+import logo from 'assets/images/logo_heytext.svg';
 /**
  * if you want to use image instead of <svg> uncomment following.
  *
@@ -12,7 +12,7 @@ import { useTheme } from '@mui/material/styles';
 // ==============================|| LOGO SVG ||============================== //
 
 const Logo = () => {
-  const theme = useTheme();
+  // const theme = useTheme();
 
   return (
     /**
@@ -21,12 +21,13 @@ const Logo = () => {
      * <img src={logo} alt="Mantis" width="100" />
      *
      */
-    <>
-      <svg width="118" height="35" viewBox="0 0 118 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <text x="10" y="25" fontFamily="Arial, sans-serif" fontSize="24" fontWeight="bold" fill="black">H</text>
-        <text x="30" y="25" fontFamily="Arial, sans-serif" fontSize="16" fill="black">heytext</text>
-      </svg>
-    </>
+    // <>
+    //   <svg width="118" height="35" viewBox="0 0 118 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+    //     <text x="10" y="25" fontFamily="Arial, sans-serif" fontSize="24" fontWeight="bold" fill="black">H</text>
+    //     <text x="30" y="25" fontFamily="Arial, sans-serif" fontSize="16" fill="black">heytext</text>
+    //   </svg>
+    // </>
+    <img src={logo} alt="Mantis" width="100" />
   );
 };
 
