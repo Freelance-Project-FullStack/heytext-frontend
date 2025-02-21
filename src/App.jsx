b7524f6e852291900c8f1be583778bff1365b6d8
PhamPhong137 <pp072003@gmail.com>
2025-02-21 23:14:36 +0700

    Fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
 // project import
 import router from './routes';
 import ThemeCustomization from 'themes';
-import { BrowserRouter } from 'react-router-dom';
+
 import ScrollTop from 'components/ScrollTop';
 import AuthCheck from './components/AuthCheck';
+import AppRoutes from './routes';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL ||============================== //
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <AuthCheck>
-        <ThemeCustomization>
-          <BrowserRouter>
-            <ScrollTop>
-              <RouterProvider router={router} />
-            </ScrollTop>
-          </BrowserRouter>
-        </ThemeCustomization>
-      </AuthCheck>
-    </Provider>
+    <AppRoutes/>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -30,12 +30,26 @@ import '@fontsource/public-sans/700.css';
 // project import
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import ThemeCustomization from 'themes';
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // ==============================|| MAIN - REACT DOM RENDER ||============================== //
 
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ThemeCustomization>
+                    <App />
+                </ThemeCustomization>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
